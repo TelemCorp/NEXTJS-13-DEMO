336748c0f1f9525f9de314afced22fb55e18297e
Zidane Innis <zidaneinnis@gmail.com>
2022-12-12 18:09:48 -0500

    Creating a Client Side Component

--- app/search/Search.tsx
+++ app/search/Search.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import React, { FormEvent, useState } from "react";
 
 function Search() {
-  return <div>Search Component</div>;
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+
+  const handleSearch = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearch("");
+    router.push(`/search/${search}`);
+  };
+
+  return (
+    <form onSubmit={handleSearch}>
+      <input
+        type="text"
+        value={search}
+        placeholder="Enter the Seacrh term"
+        onChange={(e) => setSearch(e.target.value)}
+      ></input>
+      <button
+        type="submit"
+        className="px-4 py-2 font-bold text-white bg-blue-500 rounded-lg"
+      >
+        Search
+      </button>
+    </form>
+  );
 }
 
 export default Search;
